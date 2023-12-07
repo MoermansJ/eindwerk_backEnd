0631db5479057c9b119d084fef79caa446c23d32
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2023-12-07 18:59:50 +0100

    [UPD] HighScore Controller - added getHighScoreByUsername

--- src/main/java/be/intecbrussel/eindwerk/controller/HighScoreController.java
+++ src/main/java/be/intecbrussel/eindwerk/controller/HighScoreController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,15 @@ public class HighScoreController {
     public ResponseEntity getTop10HighScores() {
         try {
             return ResponseEntity.ok(highScoreService.findTop10HighScores());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getHighScoreByUsername")
+    public ResponseEntity getHighScoreByUsername(@RequestParam String username) {
+        try {
+            return ResponseEntity.ok(highScoreService.findByUsername(username));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
